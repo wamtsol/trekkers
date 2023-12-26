69b8688d2aa0ecb28ce119be4ea798dff4e013b7
wamtbilal <54808695+wamtbilal@users.noreply.github.com>
2023-12-26 20:06:12 +0500

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,7 @@
     </div>
     <div class="pt-20 sm:pt-40 lg:pt-40 pb-20 sm:pb-20 lg:pb-20">
       <div class="text-center mx-auto w-4/6 mb-12">
-        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-white"> dfg Discover A Beautiful Place With Us</h1>
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-white">Discover A Beautiful Place With Us</h1>
         <p class="mt-6 text-lg leading-8 text-gray-600 text-white">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
       </div>
       <div class="mx-auto lg:w-4/6 md:w-4/6 w-6/6 bg-white p-4 grid grid-cols-5 gap-4 mb-3">
